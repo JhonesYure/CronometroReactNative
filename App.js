915c05095ead00d211af0abddd46905c3ea4fcf5
Jhones Yure <jymbomfim@hotmail.com>
2023-01-08 21:35:58 -0300

    up commit

---
+++ App.js
@@ -0,0 +1,29 @@
+
+import React, {Component} from 'react';
+
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+export default class App extends Component{
+ render(){
+  return (
+    <View>
+      <Text>Olá</Text>
+    </View>
+  )
+ }
+}
+
+
+//ESTILIZAÇÃO
+const styles = StyleSheet.create({
+
+})
